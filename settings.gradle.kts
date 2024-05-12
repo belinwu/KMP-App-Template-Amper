52cc8d658de77e96b0cc645ee1558ef61e6fdc30
吴上阿吉 <belinwu@qq.com>
2024-05-12 14:17:08 +0800

    Initialize demo

---
+++ settings.gradle.kts
@@ -0,0 +1,40 @@
+rootProject.name = "KMP-App-Template"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        // Amper repositories
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+        // Amper dev repo
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-559")
+}
